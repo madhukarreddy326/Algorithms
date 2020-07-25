74b940756b96a4317f53b9727bb13d9f0356aa45
Madhukar <madhukar.reddy326@gmail.com>
2020-07-25 20:52:15 +0530

    linkedlist is added

---
+++ LinkedList/First.java
@@ -0,0 +1,20 @@
+package LinkedList;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class First
+{
+
+    public static void main(String args[])
+    {
+        List<Integer> li = new LinkedList<>();
+        li.add(10);
+        li.add(20);
+        li.add(30);
+        System.out.println(li);
+
+
+        
+    }
+}
