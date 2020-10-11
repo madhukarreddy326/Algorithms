06bde09858c3751fca043c0dde7cf532d58a3a6c
Madhukar <madhukar.reddy326@gmail.com>
2020-10-11 17:24:51 +0530

    linked list to find middle element

---
+++ exercises/reverseWords.java
@@ -0,0 +1,11 @@
+package exercises;
+
+public class reverseWords
+{
+    public static void main(String args[])
+    {
+        String s = "Trees are beautiful";
+        System.out.println(s);
+
+    }
+}
